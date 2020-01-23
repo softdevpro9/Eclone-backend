d2f0cf58b02394bd960811197553baf45bb02b42
softdevpro9 <shearinmichael0@gmail.com>
2020-01-23 09:38:16 -0500

    end mvp completed

--- db/migrate/20200109140004_create_users.rb
+++ db/migrate/20200109140004_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :username
+      t.string :image_url, default: 'https://res.cloudinary.com/djxdupfed/image/upload/profile/5.jpg'
       t.text :bio
       t.string :area
       t.string :password_digest
--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_172028) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "image_url", default: "https://res.cloudinary.com/djxdupfed/image/upload/profile/5.jpg"
     t.text "bio"
     t.string "area"
     t.string "password_digest"
